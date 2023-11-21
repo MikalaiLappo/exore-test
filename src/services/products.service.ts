dcf980b7d7dfaddea35e9db73dd219aab3add46e
Mikalai Lappo <mikalailappo@yandex.by>
2023-11-21 22:41:39 +0300

    feat: init products service

---
+++ src/services/products.service.ts
@@ -0,0 +1,19 @@
+/* eslint-disable import/extensions */
+import axios from 'axios';
+import { ProductData } from '@/types/products';
+
+const BASE_URL = 'https://fakestoreapi.com/';
+const makeUrl = (s: string) => `${BASE_URL}${s}`;
+
+class ProductsService {
+  static async fetchProducts(limit: number | 'all') {
+    if (typeof limit === 'number' && limit < 1) {
+      throw new Error('ProductsService: Invalid limit parameter');
+    }
+    const url = limit === 'all' ? makeUrl('products') : makeUrl(`products?limit=${limit}`);
+    const resp = await axios.get(url);
+    return resp.data as Promise<ProductData[]>;
+  }
+}
+
+export default ProductsService;
